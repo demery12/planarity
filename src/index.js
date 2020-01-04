964dc42398f788c2aa77aefc61888f0f4372d0b4
Dylan Emery <dylane884@gmail.com>
2020-01-04 15:22:00 -0500

    Graph now showing up, but I don't like the sample I picked

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './graph.css'
-import './graph.js'
-import 'https://d3js.org/d3.v5.min.js'
+//import './graph.js'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
